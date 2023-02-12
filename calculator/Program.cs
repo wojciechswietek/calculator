48148f8cc6cd24041ad092801935eda0dd2831eb
jakubswietek <jakswietek@gmail.com>
2023-02-12 18:23:25 +0100

    Create add function.

--- calculator/Program.cs
+++ calculator/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+ int add(int x, int y)
+{
+    return x + y;
+}
+
+Console.WriteLine(add(1,2));
+Console.ReadLine();
