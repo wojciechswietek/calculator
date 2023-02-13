c7174a82d789bab4056175c94beee951e061a5d4
jakubswietek <jakswietek@gmail.com>
2023-02-13 15:41:50 +0100

    Add user parametres.

--- calculator/Program.cs
+++ calculator/Program.cs
@@ -31,3 +31,52 @@ int div(int x, int y)
 }
 
 #endregion
+
+void main()
+{
+    int a;
+    int b;
+    Console.WriteLine("Calculator");
+    Console.WriteLine("Enter first number:");
+    string A = Console.ReadLine();
+    a = int.Parse(A);
+    Console.WriteLine("Enter second number:");
+    string B = Console.ReadLine();
+    b = int.Parse(B);
+    Console.WriteLine("Witch function You want to use?");
+    string F = Console.ReadLine();
+
+    int result;
+
+    if (F == "+")
+    {
+        result = add(a, b);
+        Console.WriteLine(result);
+
+    }
+    else if (F == "-")
+    {
+        result = sub(a, b);
+        Console.WriteLine(result);
+
+    }
+    else if (F == "*")
+    {
+        result = mul(a, b);
+        Console.WriteLine(result);
+
+    }
+    else if (F == "/")
+    {
+        result = div(a, b);
+        Console.WriteLine(result);
+
+    }
+    else
+    {
+        Console.WriteLine("Wrong function");
+    }
+}
+
+
+main();
