a78e34c7302b053d8022ad4b6a9fe96e5eaceef6
JakSwietek <125198857+JakSwietek@users.noreply.github.com>
2023-02-13 16:13:21 +0100

    5 feature get parameters from user (#11)
    * Add user parametres.
    
    * Refactor main function.

--- calculator/Program.cs
+++ calculator/Program.cs
@@ -31,3 +31,51 @@ int div(int x, int y)
 }
 
 #endregion
+
+void main()
+{
+
+    Console.WriteLine("Calculator");
+    Console.WriteLine("Enter first number:");
+    string firstInput = Console.ReadLine();
+    int firstNumber = int.Parse(firstInput);
+    Console.WriteLine("Enter second number:");
+    string secondInput = Console.ReadLine();
+    int secondNumber = int.Parse(secondInput);
+    Console.WriteLine("Which function You want to use?");
+    string mathOperation = Console.ReadLine();
+
+    int result;
+
+    if (mathOperation == "+")
+    {
+        result = add(firstNumber, secondNumber);
+        Console.WriteLine(result);
+
+    }
+    else if (mathOperation == "-")
+    {
+        result = sub(firstNumber, secondNumber);
+        Console.WriteLine(result);
+
+    }
+    else if (mathOperation == "*")
+    {
+        result = mul(firstNumber, secondNumber);
+        Console.WriteLine(result);
+
+    }
+    else if (mathOperation == "/")
+    {
+        result = div(firstNumber, secondNumber);
+        Console.WriteLine(result);
+
+    }
+    else
+    {
+        Console.WriteLine("Wrong function");
+    }
+}
+
+
+main();
