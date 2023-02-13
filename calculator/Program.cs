f75dcf05b75a4d72239e36ae237289d6ba80dfaf
jakubswietek <jakswietek@gmail.com>
2023-02-13 10:59:05 +0100

    Create div mul sub functions.

--- calculator/Program.cs
+++ calculator/Program.cs
@@ -1,7 +1,67 @@
-﻿ int add(int x, int y)
+﻿#region add function 
+
+int add(int x, int y)
 {
     return x + y;
 }
 
-Console.WriteLine(add(1,2));
-Console.ReadLine();
+#endregion
+
+#region sub function
+int sub(int x, int y)
+{
+    return x - y;
+}
+
+#endregion
+
+#region mul function
+int mul(int x, int y)
+{
+    return x * y;
+}
+
+#endregion
+
+#region div function
+
+int div(int x, int y)
+{
+    return x / y;
+}
+
+#endregion
+/*
+int main(int a, int b)
+{ }
+int a;
+int b;
+Console.WriteLine("Calculator");
+Console.WriteLine("Enter first number:");
+string A = Console.ReadLine();
+a = int.Parse(A);
+Console.WriteLine("Enter second number:");
+string B = Console.ReadLine();
+b = int.Parse(B);
+Console.WriteLine("Witch function You want to use?");
+string F = Console.ReadLine();
+if (F == +)
+{
+    int add;
+}
+if (F == -)
+{
+    int sub;
+}
+if (F == *)
+{
+    int mul;
+}
+if (F == /)
+{
+    int div;
+}
+else
+    Console.WriteLine("Wrong function");
+}
+*/
