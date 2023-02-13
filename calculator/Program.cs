34da06585b9776dc7b5d1d9a2be8f5bfb2a28f44
jakubswietek <jakswietek@gmail.com>
2023-02-13 11:48:37 +0100

    Comment remóved.

--- calculator/Program.cs
+++ calculator/Program.cs
@@ -31,37 +31,3 @@ int div(int x, int y)
 }
 
 #endregion
-/*
-int main(int a, int b)
-{ }
-int a;
-int b;
-Console.WriteLine("Calculator");
-Console.WriteLine("Enter first number:");
-string A = Console.ReadLine();
-a = int.Parse(A);
-Console.WriteLine("Enter second number:");
-string B = Console.ReadLine();
-b = int.Parse(B);
-Console.WriteLine("Witch function You want to use?");
-string F = Console.ReadLine();
-if (F == +)
-{
-    int add;
-}
-if (F == -)
-{
-    int sub;
-}
-if (F == *)
-{
-    int mul;
-}
-if (F == /)
-{
-    int div;
-}
-else
-    Console.WriteLine("Wrong function");
-}
-*/
